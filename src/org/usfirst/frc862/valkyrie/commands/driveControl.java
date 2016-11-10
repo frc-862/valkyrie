d5b94e9eff3d7db25b6acc3288cf788b04bdeda2
EPelk <roboguy365@gmail.com>
2016-11-09 19:48:45 -0500

    Set default command for DriveTrain.java

--- src/org/usfirst/frc862/valkyrie/commands/driveControl.java
+++ src/org/usfirst/frc862/valkyrie/commands/driveControl.java
@@ -43,12 +43,12 @@ public class driveControl extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.driveControl(Robot.oi.gamepad.getJoystickY(0),Robot.oi.gamepad.getJoystickY(1));
+    	Robot.driveTrain.driveControl(-Robot.oi.gamepad.getRawAxis(1),-Robot.oi.gamepad.getRawAxis(5));
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true

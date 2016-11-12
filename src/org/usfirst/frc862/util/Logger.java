53a6d0f19a80e8105af4b863ccd5a2f27e652b17
Patrick Hurley <phurley@gmail.com>
2016-11-11 19:58:08 -0500

    Add handy utility logger class

---
+++ src/org/usfirst/frc862/util/Logger.java
@@ -0,0 +1,101 @@
+package org.usfirst.frc862.util;
+
+import edu.wpi.first.wpilibj.Timer;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Logger {
+    public static final double FREQUENCY = 0.5;
+    public static final int BUFFER_DEPTH = 500;
+
+    private static Logger logger;
+    private Thread logging_thread;
+
+    private File file = null;
+    private BufferedWriter writer;
+    private ArrayBlockingQueue<String> buffer = new ArrayBlockingQueue<String>(BUFFER_DEPTH);
+    private double next_write = Timer.getFPGATimestamp() + FREQUENCY;
+
+    private static Logger getLogger() {
+        if (logger == null) {
+            logger = new Logger();
+        }
+        return logger;
+    }
+
+    void write_buffered_message() {
+        try {
+            writer.write(buffer.take());
+            writer.newLine();
+
+            if (Timer.getFPGATimestamp() > next_write) {
+                writer.flush();
+                next_write = Timer.getFPGATimestamp() + FREQUENCY;
+            }
+        } catch (Exception e) {
+            System.err.println("Error writing buffer");
+            e.printStackTrace();
+        }
+    }
+
+    private Logger() {
+        try {
+            file = logFileName();
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
+
+            logging_thread = new Thread(() -> {
+                while (true) {
+                    write_buffered_message();
+                }
+            });
+            logging_thread.setPriority(Thread.MIN_PRIORITY);
+            logging_thread.setName("LoggingThread");
+            logging_thread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    protected File logFileName() {
+        File base = null;
+
+        // find the mount point
+        char mount = 'u';
+        while (base == null && mount <= 'z') {
+            File f = new File("/" + mount);
+            if (f.isDirectory()) {
+                base = f;
+            }
+            ++mount;
+        }
+
+        if (base == null) {
+            base = new File("/home/lvuser");
+        }
+
+        base = new File(base, "log");
+        base.mkdirs();
+
+        int counter = 0;
+        File result = new File(base, String.format("robot-%05d.log", counter));
+        while (result.exists()) {
+            result = new File(base, String.format("robot-%05d.log", ++counter));
+        }
+
+        return result;
+    }
+
+    private boolean logString(String s) {
+        return buffer.offer(s);
+    }
+
+    public static boolean log(String s) {
+        return getLogger().logString(s);
+    }
+}

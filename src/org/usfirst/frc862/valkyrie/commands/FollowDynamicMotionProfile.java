9deb6526cbd5a6891e7a7bc60a8e6528071635e6
Luke Fenstermacher <luke.fenstermacher@gmail.com>
2017-02-01 20:57:03 -0500

    Test
    Test

--- src/org/usfirst/frc862/valkyrie/commands/FollowDynamicMotionProfile.java
+++ src/org/usfirst/frc862/valkyrie/commands/FollowDynamicMotionProfile.java
@@ -90,7 +90,7 @@ public class FollowDynamicMotionProfile extends Command {
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
     	//return index >= left.length();
-    	return false;
+    	return left.isFinished() && right.isFinished();
     }
 
     // Called once after isFinished returns true

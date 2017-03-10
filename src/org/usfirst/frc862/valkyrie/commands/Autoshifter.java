dc0cd4029304e4a5e4af7760c251ce05d16356cb
Nikolas Stojcevich <nstojcevich@gmail.com>
2017-03-10 18:18:04 -0500

    add hasGear signal with LED ring

--- src/org/usfirst/frc862/valkyrie/commands/Autoshifter.java
+++ src/org/usfirst/frc862/valkyrie/commands/Autoshifter.java
@@ -112,7 +112,7 @@ public class Autoshifter extends Command {
             break;
 
         case UP_SHIFTING:
-            Robot.driveTrain.slowForSeconds(0.250);
+            Robot.driveTrain.slowForSeconds(0.10);
             originalRDistance = Robot.driveTrain.getRightDistance();
             originalLDistance = Robot.driveTrain.getLeftDistance();
             Robot.shifter.upShift();

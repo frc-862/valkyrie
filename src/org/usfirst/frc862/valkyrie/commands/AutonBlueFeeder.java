7ad390b27a735eb815606ffadc3205945a81bf87
Nikolas Stojcevich <nstojcevich@gmail.com>
2017-03-10 19:46:39 -0500

    calibrate blue feeder auton

--- src/org/usfirst/frc862/valkyrie/commands/AutonBlueFeeder.java
+++ src/org/usfirst/frc862/valkyrie/commands/AutonBlueFeeder.java
@@ -37,7 +37,7 @@ public class AutonBlueFeeder extends DynamicPathCommand {
         testing = false;
         reverse = false;
         addWaypoint(0,0,0);
-        addWaypoint(7.85,6.8,70);
+        addWaypoint(8.3,6.8,70);
 
     }
 
